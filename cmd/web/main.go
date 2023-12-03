3c90affa5d0007bcb0e0016bd1efef1117b5c8d9
Soul-Remix <soulremix-dev@pm.me>
2023-12-03 15:54:15 +0200

    Move routes to separate file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,22 +7,13 @@ import (
 	"os"
 )
 
-type config struct {
-	addr      string
-	staticDir string
-}
-
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
 }
 
 func main() {
-	var cfg config
-
-	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
-	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
-
+	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
@@ -33,19 +24,10 @@ func main() {
 		errorLog: errorLog,
 	}
 
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir(cfg.staticDir))
-
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
 	srv := &http.Server{
-		Addr:     cfg.addr,
+		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	infoLog.Print("Starting server on :4000")
