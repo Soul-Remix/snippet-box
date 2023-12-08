b0ef9e612cad731121e16ba2b9d83a61dfa02e13
Soul-Remix <soulremix-dev@pm.me>
2023-12-08 20:29:58 +0200

    Implement isAuthenticated middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,26 +9,27 @@ import (
 
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
-
 	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		app.notFound(w)
 	})
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	router.Handler(http.MethodGet, "/static/*file", http.StripPrefix("/static", fileServer))
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-	router.HandlerFunc(http.MethodGet, "/", app.home)
+	dynamic := alice.New(app.sessionManager.LoadAndSave)
+	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
+	router.Handler(http.MethodGet, "/snippet/view/:id", dynamic.ThenFunc(app.snippetView))
+	router.Handler(http.MethodGet, "/user/signup", dynamic.ThenFunc(app.userSignupForm))
+	router.Handler(http.MethodPost, "/user/signup", dynamic.ThenFunc(app.userSignupPost))
+	router.Handler(http.MethodGet, "/user/login", dynamic.ThenFunc(app.userLoginForm))
+	router.Handler(http.MethodPost, "/user/login", dynamic.ThenFunc(app.userLoginPost))
 
-	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
-	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreateForm)
-	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
+	protected := dynamic.Append(app.requireAuthentication)
+	router.Handler(http.MethodGet, "/snippet/create", protected.ThenFunc(app.snippetCreateForm))
+	router.Handler(http.MethodPost, "/snippet/create", protected.ThenFunc(app.snippetCreatePost))
+	router.Handler(http.MethodPost, "/user/logout", protected.ThenFunc(app.userLogoutPost))
 
-	router.HandlerFunc(http.MethodGet, "/user/signup", app.userSignupForm)
-	router.HandlerFunc(http.MethodPost, "/user/signup", app.userSignupPost)
-	router.HandlerFunc(http.MethodGet, "/user/login", app.userLoginForm)
-	router.HandlerFunc(http.MethodPost, "/user/login", app.userLoginPost)
-	router.HandlerFunc(http.MethodPost, "/user/logout", app.userLogoutPost)
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
-	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders, app.sessionManager.LoadAndSave)
 	return standard.Then(router)
 }
