82838748ae8b0e319ca3beb9ba9553ffd8d10bea
Soul-Remix <soulremix-dev@pm.me>
2023-12-15 16:19:20 +0200

    Update validateInt to use generics

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -79,7 +79,7 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	form.CheckField(validator.NotBlank(form.Title), "title", "This field should not be blank")
 	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field should not be more than 100 characters")
 	form.CheckField(validator.NotBlank(form.Content), "content", "This field should not be blank")
-	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "title", "This field should be one of this values: 1, 7 or 365")
+	form.CheckField(validator.PermittedValue(form.Expires, 1, 7, 365), "title", "This field should be one of this values: 1, 7 or 365")
 
 	if !form.Valid() {
 		data := app.newTemplateData(r)
