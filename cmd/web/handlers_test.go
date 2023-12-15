68026d3486093ffd3f8df04af93d24f012fafc2c
Soul-Remix <soulremix-dev@pm.me>
2023-12-15 17:41:09 +0200

    Add e2e testing and test utils

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -33,3 +33,15 @@ func TestPing(t *testing.T) {
 
 	assert.Equal(t, string(body), "OK")
 }
+
+func TestPingE2e(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	code, _, body := ts.get(t, "/ping")
+
+	assert.Equal(t, code, http.StatusOK)
+	assert.Equal(t, body, "OK")
+}
